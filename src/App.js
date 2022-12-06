d181e4538ecf0651fc4fa71aad55eeec19d7441a
Ma1ve <93385505+Ma1ve@users.noreply.github.com>
2022-12-06 12:56:04 +0300

    pagination

--- src/App.js
+++ src/App.js
@@ -2,61 +2,20 @@ import React, { useState, useMemo } from 'react';
 
 import './styles/App.css';
 
-import PostList from './component/PostList';
-import PostForm from './component/PostForm';
-import MySelect from './UI/select/MySelect';
-import MyInput from './UI/input/MyInput';
-import PostFilter from './component/PostFilter';
-import MyModal from './UI/MyModal/MyModal';
-import MyButton from './UI/button/MyButton';
-import { usePosts } from './hooks/usePosts';
+import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom';
+import About from './pages/About';
+import Posts from './pages/Posts';
+import Navbar from './component/Navbar';
+import Error from './pages/Error';
+import AppRouter from './UI/AppRouter/AppRouter';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'аа', body: 'бб' },
-    { id: 2, title: 'гг 2', body: 'аа' },
-    { id: 3, title: 'вв 3', body: 'яя' },
-  ]);
-
-  const [filter, setFilter] = useState({ sort: '', query: '' });
-  const [modal, setModal] = useState(false);
-
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  // const sortedPosts = useMemo(() => {
-  //   if (filter.sort) {
-  //     return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-  //   }
-  //   return posts;
-  // }, [filter.sort, posts]);
-
-  // const sortedAndSearchedPosts = useMemo(() => {
-  //   return sortedPosts.filter((post) => post.title.toLowerCase().includes(filter.query));
-  // }, [filter.query, sortedPosts]);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
   return (
     <>
-      <div className="App">
-        <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
-          Создать пользователя
-        </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
-          <PostForm create={createPost} />
-        </MyModal>
-
-        <hr style={{ margin: '15px 0' }} />
-        <PostFilter filter={filter} setFilter={setFilter} />
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <div>Это работает!@!!</div>
+    </>
+  );
+};
+
+export default About;
